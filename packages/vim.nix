49870eebe8c99a0096a4cd1a66055dd81f3effb1
hurbeana <hurbeana@gmail.com>
2024-08-22 17:34:41 +0200

    moved files and import

---
+++ packages/vim.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  home = {
+    packages = with pkgs; [
+      vim
+    ];
+
+  programs = {
+
+    vim = {
+      enable = true;
+      defaultEditor = true;
+      settings = {
+        expandtab = true;
+        mouse = null;
+        number = true;
+        relativenumber = true;
+        shiftwidth = 2;
+        tabstop = 2;
+        history = 1000;
+      };
+      extraConfig = ''
+        set autoindent
+        set backspace=indent,eol,start
+        syntax on
+      '';
+    };
+
+  };
+}
