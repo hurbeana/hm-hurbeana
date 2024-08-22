46c4883e5f0c4193b4556a49ec69d7ba115bc390
hurbeana <hurbeana@gmail.com>
2024-08-22 16:49:50 +0200

    finished vim config

--- home.nix
+++ home.nix
@@ -12,6 +12,38 @@ in {
     stateVersion = "24.05";
   };
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+  programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+
+    # bash
+    bash = {
+      enable = true;
+      enableCompletion = true;
+      shellAliases = {
+        "ll" = "ls -l";
+        ".." = "cd ..";
+      };
+    };
+
+    # vim config
+    vim = {
+      enable = true;
+      defaultEditor = true;
+      settings = {
+        expandtab = true;
+        mouse = null;
+        number = true;
+        relativenumber = true;
+        shiftwidth = 2;
+        tabstop = 2;
+        history = 1000;
+      };
+      extraConfig = ''
+        set autoindent
+        set backspace=indent,eol,start
+        syntax on
+      '';
+    };
+  };
 }
