ab9da23d6c5803207f342dcd56cc90043649dd89
hurbeana <hurbeana@gmail.com>
2024-08-22 17:54:28 +0200

    added code.nix

---
+++ packages/code.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+{
+  programs = {
+    vscode = {
+      enable = true;
+      enableExtensionUpdateCheck = true;
+      enableUpdateCheck = true;
+      extensions = with pkgs.vscode-extensions; [
+        ms-python.python
+        ms-python.debugpy
+        ms-python.black-formatter
+        ms-python.isort
+        vscodevim.vim
+        james-yu.latex-workshop
+        oderwat.indent-rainbow
+        github.copilot
+        github.copilot-chat
+        shd101wyy.markdown-preview-enhanced
+        eamodio.gitlens
+        ibm.output-colorizer
+        esbenp.prettier-vscode
+        mechatroner.rainbow-csv
+      ];
+  };
+}
