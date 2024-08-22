91082e139bc6990255a9cf5ea6bec4fa54a0325e
hurbeana <hurbeana@gmail.com>
2024-08-22 15:50:13 +0200

    revision of home.nix

--- home.nix
+++ home.nix
@@ -1,14 +1,17 @@
-{ lib, pkgs, ... }:
-{
+{ lib, pkgs, ... }: let
+  username = "hurbeana";
+in {
   home = {
     packages = with pkgs; [
       vscode
       texliveMedium
     ];
 
-    username = "hurbeana";
-    homeDirectory = "/home/hurbeana";
-
+    inherit username;
+    homeDirectory = "/home/${username}";
     stateVersion = "24.05";
   };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
 }
